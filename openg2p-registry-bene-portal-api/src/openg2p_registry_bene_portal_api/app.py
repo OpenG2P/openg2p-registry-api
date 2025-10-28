be9cae9e92e6a15af59cabcee1bcb69afd2f3619
Manoj Kumar <mkumar6@ch.iitr.ac.in>
2025-10-28 10:34:30 +0530

    Base structure
    Signed-off-by: Manoj Kumar <mkumar6@ch.iitr.ac.in>

--- openg2p-registry-bene-portal-api/src/openg2p_registry_bene_portal_api/app.py
+++ openg2p-registry-bene-portal-api/src/openg2p_registry_bene_portal_api/app.py
@@ -4,8 +4,6 @@ import logging
 from openg2p_fastapi_common.app import Initializer as BaseInitializer
 
 from .config import Settings
-from .controllers import RegistryController
-from .services import RegistryService
 
 _config = Settings.get_config()
 _logger = logging.getLogger(_config.logging_default_logger_name)
@@ -14,6 +12,3 @@ _logger = logging.getLogger(_config.logging_default_logger_name)
 class Initializer(BaseInitializer):
     def initialize(self, **kwargs):
         super().initialize()
-
-        RegistryService()
-        RegistryController().post_init()
